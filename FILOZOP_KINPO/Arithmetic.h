ef41f518bd50c278c1e12f94c462d8a24229e92b
filozop <polinafilozop@gmail.com>
2026-06-09 19:58:16 +0300

    feat: add prependZerosLeft method

--- FILOZOP_KINPO/Arithmetic.h
+++ FILOZOP_KINPO/Arithmetic.h
@@ -9,6 +9,7 @@ private:
 	vector<uint8_t> fractionPart;
 	bool isNegative;
 	void appendZerosRight(std::vector<uint8_t>& vec, size_t targetLen) const;
+	void prependZerosLeft(std::vector<uint8_t>& vec, size_t targetLen) const;
 
 public:
 	FractionNumber() {
